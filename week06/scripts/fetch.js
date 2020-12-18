0181a8b93fd756904d990985d8c89b93f1c934e8
Elliott Cheah <elaine_manx@hotmail.com>
2020-12-18 19:47:41 +0800

    1947

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -80,3 +80,9 @@ function DeleteBooking(id) {
             GetBookings();
         });
 }
+
+let deleteBooking = document.getElementById("deleteBooking");
+deleteBooking.addEventListener("click", function () {
+    let deleteId = document.getElementById("deleteId").value;
+    DeleteBooking(deleteId);
+});
